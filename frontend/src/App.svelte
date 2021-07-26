b8f7c68029f54a38b259c3313e39dbfee46a31ca
Henri Vandersleyen <hvandersleyen@gmail.com>
2021-07-26 15:28:26 -0700

    frontend vite jest tailwind

---
+++ frontend/src/App.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import logo from './assets/svelte.png'
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main class="bg-gray-900">
+  <img src={logo} alt="Svelte Logo" />
+  <h1>Hello Typescript!</h1>
+
+  <Counter />
+  <div class="bg-yellow-300">
+
+  <p>
+    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
+    apps.
+  </p>
+
+  <p>
+    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
+    the officially supported framework, also powered by Vite!
+  </p>
+  </div>
+</main>
+
+<style>
+
+</style>
