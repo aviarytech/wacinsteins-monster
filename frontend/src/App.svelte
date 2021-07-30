5bb03e14ef5a350130943b65e0778f22a5aff115
Henri Vandersleyen <hvandersleyen@gmail.com>
2021-07-30 11:38:04 -0700

    added 2 comments regarding bugs with heroicons

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -13,7 +13,7 @@
   import { Router, Route} from "svelte-navigator";
 </script>
 
-<main class="bg-gray-900">
+<template class="">
   <!-- <img src={logo} alt="Svelte Logo" /> -->
   <div class="h-screen flex overflow-hidden bg-gray-100">
     <Sidebar/>
@@ -27,17 +27,19 @@
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" />
         </svg>
       </button>
+
       <div class="flex-1 px-4 flex justify-between">
+
         <div class="flex-1 flex">
         <SearchBar/>
         </div>
+
         <div class="ml-4 flex items-center md:ml-6">
           <button class="bg-white p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
             <span class="sr-only">View notifications</span>
-            <!-- Heroicon name: outline/bell -->
-            <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
-            </svg>
+            <!--BUG:bell not showing unless I add a letter? -->
+            <img src="./assets/outlineBell.svg" alt='notification'/>
+            u
           </button>
           <Profile/>
 
@@ -76,15 +78,15 @@
           <!-- Replace with your content -->
           <div class="py-4">
             <div class="border-4 border-dashed border-gray-200 rounded-lg h-96"></div>
-          </div>
+            </div>
           <!-- /End replace -->
+          </div>
         </div>
-      </div>
     </main>
   </div>
 </div>
-</main>
+</template>
 
-<style>
+<style lang="postcss">
 
 </style>
--- frontend/src/lib/SearchBar.svelte
+++ frontend/src/lib/SearchBar.svelte
@@ -9,8 +9,9 @@
     <div class="relative w-full text-gray-400 focus-within:text-gray-600">
       <div class="absolute inset-y-0 left-0 flex items-center pointer-events-none">
         <!-- Heroicon name: outline/document search -->
-        <!--BUG: the icon doesn't show -->
+        <!--BUG: the icon doesn't unless there's a letter-->
         <img src='../assets/outlineDocumentSearch.svg' alt="heroIcon"/>
+        
 
       </div>
       <input id="search-field" class="block w-full h-full pl-8 pr-3 py-2 border-transparent text-gray-900 placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-0 focus:border-transparent sm:text-sm" placeholder="Search" type="search" name="search">
