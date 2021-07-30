e1972eecdfc2fe158e42b98c1dc1ef53b78c1dbd
Henri Vandersleyen <hvandersleyen@gmail.com>
2021-07-30 09:37:50 -0700

    added one bug .svg icon not showing but separated the searchbar.

---
+++ frontend/src/lib/SearchBar.svelte
@@ -0,0 +1,22 @@
+<script lant="ts">
+
+
+</script>
+
+<template>
+  <form class="w-full flex md:ml-0" action="#" method="GET">
+    <label for="search-field" class="sr-only">Search</label>
+    <div class="relative w-full text-gray-400 focus-within:text-gray-600">
+      <div class="absolute inset-y-0 left-0 flex items-center pointer-events-none">
+        <!-- Heroicon name: outline/document search -->
+        <!--BUG: the icon doesn't show -->
+        <img src='../assets/outlineDocumentSearch.svg' alt="heroIcon"/>
+
+      </div>
+      <input id="search-field" class="block w-full h-full pl-8 pr-3 py-2 border-transparent text-gray-900 placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-0 focus:border-transparent sm:text-sm" placeholder="Search" type="search" name="search">
+    </div>
+  </form>
+</template>
+
+<style lang="postcss">
+</style>
