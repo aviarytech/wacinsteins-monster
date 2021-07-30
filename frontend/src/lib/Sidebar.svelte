0083f62d9ac3c79f0d731c28cd05e4d98b22ef49
Henri Vandersleyen <hvandersleyen@gmail.com>
2021-07-30 10:05:30 -0700

    fixed the mobile sidebar layout

--- frontend/src/lib/Sidebar.svelte
+++ frontend/src/lib/Sidebar.svelte
@@ -4,6 +4,77 @@
 </script>
 
 <template>
+<!-- Off-canvas menu for mobile, show/hide based on off-canvas menu state. -->
+  <div class="fixed inset-0 flex z-40 md:hidden" role="dialog" aria-modal="true">
+    <!--
+      Off-canvas menu overlay, show/hide based on off-canvas menu state.
+
+      Entering: "transition-opacity ease-linear duration-300"
+        From: "opacity-0"
+        To: "opacity-100"
+      Leaving: "transition-opacity ease-linear duration-300"
+        From: "opacity-100"
+        To: "opacity-0"
+    -->
+    <div class="fixed inset-0 bg-gray-600 bg-opacity-75" aria-hidden="true"></div>
+
+    <!--
+      Off-canvas menu, show/hide based on off-canvas menu state.
+
+      Entering: "transition ease-in-out duration-300 transform"
+        From: "-translate-x-full"
+        To: "translate-x-0"
+      Leaving: "transition ease-in-out duration-300 transform"
+        From: "translate-x-0"
+        To: "-translate-x-full"
+    -->
+    <div class="relative flex-1 flex flex-col max-w-xs w-full pt-5 pb-4 bg-white">
+      <!--
+        Close button, show/hide based on off-canvas menu state.
+
+        Entering: "ease-in-out duration-300"
+          From: "opacity-0"
+          To: "opacity-100"
+        Leaving: "ease-in-out duration-300"
+          From: "opacity-100"
+          To: "opacity-0"
+      -->
+      <div class="absolute top-0 right-0 -mr-12 pt-2">
+        <button class="ml-1 flex items-center justify-center h-10 w-10 rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+          <span class="sr-only">Close sidebar</span>
+          <!-- Heroicon name: outline/x -->
+          <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+          </svg>
+        </button>
+      </div>
+
+      <div class="flex-shrink-0 flex items-center px-4">
+        <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-logo-indigo-600-mark-gray-800-text.svg" alt="Workflow">
+      </div>
+      <div class="mt-5 flex-1 h-0 overflow-y-auto">
+        <nav class="px-2 space-y-1">
+          <Router>
+            {#each Routes as route}
+              {#if route.heroIcon !== ''}
+
+                <div class="text-gray-600 hover:bg-gray-50 hover:text-gray-900 group flex items-center px-2 py-2 text-sm font-medium rounded-md">
+                  <Link to={route.routeUrl}>
+                    <img src={route.heroIcon} alt="heroIcon"/>
+                    {route.routeName}
+                  </Link>
+                </div>
+              {/if}
+            {/each}
+          </Router>
+      </div>
+    </div>
+
+    <div class="flex-shrink-0 w-14" aria-hidden="true">
+      <!-- Dummy element to force sidebar to shrink to fit close icon -->
+    </div>
+  </div>
+
 <!-- Static sidebar for desktop -->
   <div class="hidden md:flex md:flex-shrink-0">
     <div class="flex flex-col w-64">
