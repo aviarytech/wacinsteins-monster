1a6c425298d7c1f1df7e3a8e41bdce5744f2358b
Henri Vandersleyen <hvandersleyen@gmail.com>
2021-08-04 16:44:41 -0700

    started to wrap my head around json-ld but need mentoring to understand better the ecosystem it must interact with.

--- frontend/src/routes/Credentials.svelte
+++ frontend/src/routes/Credentials.svelte
@@ -1,5 +1,16 @@
 <script lang="ts">
+  import {Person, WithContext} from 'schema-dts';
 
+  const p: WithContext<Person> = {
+    '@context': 'https://schema.org',
+    '@type': 'Person',
+    name: 'Eve',
+    affiliation: {
+      '@type': 'School',
+      name: 'Nice School',
+    },
+  };
+  console.log(p)
 </script>
 
 <template>
