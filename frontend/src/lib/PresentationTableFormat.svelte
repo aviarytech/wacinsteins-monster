4d45c2c73840669d504297b9323a4ea60e4136f0
Henri Vandersleyen <hvandersleyen@gmail.com>
2021-08-05 16:04:33 -0700

    another small change to mockend

--- frontend/src/lib/PresentationTableFormat.svelte
+++ frontend/src/lib/PresentationTableFormat.svelte
@@ -8,6 +8,7 @@
       <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
         <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
           <table class="min-w-full divide-y divide-gray-200">
+            <!--WARN: MOCKEND API TEST GENERATED INDEX STARTS AT INDEX 1-->
             {#if rowId === 1}
               <thead class="bg-gray-50">
                 <tr>
