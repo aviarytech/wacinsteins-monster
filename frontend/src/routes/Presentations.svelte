e0172cfee71be065fd15379924ca639e5268485a
Henri Vandersleyen <hvandersleyen@gmail.com>
2021-08-05 12:25:10 -0700

    added a sample api using axios. need to find the specific api fields and implement it.

--- frontend/src/routes/Presentations.svelte
+++ frontend/src/routes/Presentations.svelte
@@ -1,9 +1,46 @@
 <script lang="ts">
+  //imports
+  import { getCall } from "../api/presentationAxios";
 
+
+//TODO: query backend for presentation
+// axios/getall 
+
+  async function apiGetCall():Promise<any>{
+      let res = await getCall('https://jsonplaceholder.typicode.com/todos/1')
+      console.log(res)
+      return res
+  }
+  let displayData = false
+  interface SampleApi {
+      userId:number;
+      id:number;
+      title:string;
+      completed:Boolean;
+    }
+  let data:SampleApi[] = []
+  $: if (displayData === true){
+      (async() => {
+        const res = await apiGetCall()
+        data = res 
+        })()
+    } 
 </script>
 
 <template>
-presentation
+  <div>
+    <button on:click={() => {displayData = !displayData}}>
+      click me
+    </button>
+  </div>
+
+  {#if displayData === true}
+    <ul>
+    {#each Object.values(data) as value }
+      <li>{value}</li>
+    {/each}
+    </ul>
+  {/if}
 </template>
 
 <style lang="postcss">
