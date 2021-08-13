d6cf834c8ef904639d387b3f62297b3c0b4b5bac
Brian Richter <brianorwhatever@gmail.com>
2021-08-13 12:30:19 -0700

    fix merge conflicts

--- frontend/src/routes/Credentials.svelte
+++ frontend/src/routes/Credentials.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
-import CredentialDetailView from "../lib/CredentialDetailView.svelte";
-import DataTable from "../lib/DataTable.svelte";
-import { onMount } from "svelte";
-
+  import CredentialDetailView from "../lib/CredentialDetailView.svelte";
+  import DataTable from "../lib/DataTable.svelte";
+  import { onMount } from "svelte";
 
   import { credentials } from "../stores/credentials";
   import { slideOverContent } from "../stores/ui";
@@ -32,33 +31,22 @@ import { onMount } from "svelte";
     if (resp.length > 0) {
       credentials.set(resp);
     }
-
   });
-};
-
-onMount(async () => {
-  const resp = await getAllCredentials();
-  if (resp.length > 0) {
-    credentials.set(resp);
-  }
-});
 </script>
 
 <template>
   {#each $credentials as row, i}
-    <DataTable
-      columns="{['ID', 'Issuance Date', 'Issuer', 'Name']}"
-      rowId="{i}">
-      <button on:click="{() => openCredential(row['@id'])}" slot="name"
-        >{row.data.id}</button>
+    <DataTable columns={["ID", "Issuance Date", "Issuer", "Name"]} rowId={i}>
+      <button on:click={() => openCredential(row["@id"])} slot="name"
+        >{row.data.id}</button
+      >
       <span slot="jobTitle">{row.data.issuanceDate}</span>
       <span slot="email">{row.data.issuer.id}</span>
       <span slot="role">{row.data.name}</span>
       <span slot="actions"
         ><button
-          on:click="{() => openCredential(row['@id'])}"
+          on:click={() => openCredential(row["@id"])}
           type="button"
-
           class="button"
         >
           View
@@ -67,7 +55,6 @@ onMount(async () => {
           class="button">New Presentation</button
         >
       </span>
-
     </DataTable>
   {/each}
 </template>
