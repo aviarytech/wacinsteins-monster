e25c130eb680efd3b771af8b41844fe1ee8106af
Brian Richter <brianorwhatever@gmail.com>
2021-08-30 10:31:36 -0700

    remove commented out user code

--- frontend/src/lib/AuthGuard.svelte
+++ frontend/src/lib/AuthGuard.svelte
@@ -1,29 +1,6 @@
 <script lang="ts">
-  import { user } from "../stores/user";
-  import { Magic } from "magic-sdk";
-  import { onMount } from "svelte";
-
-  // export const MAGIC_API_KEY = import.meta.env.VITE_MAGIC as string;
-  // const magic = new Magic(MAGIC_API_KEY);
-
-  // const checkLoginStatus = async () => {
-  //   if ($user) {
-  //     return;
-  //   }
-  //   if (await magic.user.isLoggedIn()) {
-  //     const meta = await magic.user.getMetadata();
-  //     user.set({ email: meta.email });
-  //   } else {
-  //     user.set(null);
-  //   }
-  // };
-
-  // onMount(async () => {
-  //   checkLoginStatus();
-  // });
+import { user } from "../stores/user";
 </script>
-
-<!-- <pre>{JSON.stringify($user, null, 2)}</pre> -->
 
 {#if $user}
   <slot name="authed">
@@ -34,4 +11,3 @@
     <em>not_authed</em>
   </slot>
 {/if}
-<!-- <slot><h1>test</h1></slot> -->
