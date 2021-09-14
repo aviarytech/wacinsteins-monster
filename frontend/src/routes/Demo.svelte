17ecf76825599388d1a602052df8187a47593d7a
Brian Richter <brianorwhatever@gmail.com>
2021-09-13 23:45:28 -0700

    gm

--- frontend/src/routes/Demo.svelte
+++ frontend/src/routes/Demo.svelte
@@ -4,7 +4,7 @@
     <iframe
       width="560"
       height="315"
-      src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1"
+      src="https://www.youtube.com/embed/0EuQgr5qieA?autoplay=1"
       title="YouTube video player"
       frameborder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
