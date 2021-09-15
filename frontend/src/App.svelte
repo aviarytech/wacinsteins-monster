c022ce8dc9843c54447e32611d0432af9b898b80
Henri Vandersleyen <hvandersleyen@gmail.com>
2021-09-15 11:52:25 -0700

    moved one svg into a file and cleaned a bit.

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -43,21 +43,11 @@ onMount(async () => {
         <button
           class="px-4 border-r border-gray-200 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 md:hidden">
           <span class="sr-only">Open sidebar</span>
-          <svg
-            class="h-6 w-6"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            aria-hidden="true">
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M4 6h16M4 12h16M4 18h7"></path>
-          </svg>
+          <img
+            src="../assets/svg/outlineTripleSidebarMobile.svg"
+            class="icon md:px-1"
+            alt="heroIcon" />
         </button>
-
         <div class="flex-1 px-4 flex justify-between">
           <div class="flex-1 flex">
             <!-- <SearchBar /> -->
