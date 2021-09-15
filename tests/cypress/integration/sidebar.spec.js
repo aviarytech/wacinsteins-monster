c5e31ea83fb8fab66dd4f8931ec16b6412eda376
Henri Vandersleyen <hvandersleyen@gmail.com>
2021-09-15 12:18:50 -0700

    cleared cache and added on the ignore list the videos and screenshots folders

---
+++ tests/cypress/integration/sidebar.spec.js
@@ -0,0 +1,41 @@
+describe("Presentation schema", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+  // future test to confirm that the sidebar is rendered correctly
+  // confirming the element is rendered proprely
+  it("the sidebar renders proprely", () => {
+    // elements of the sidebar
+    cy.wrap(["Credentials", "Presentation", "Messages", "Connections"]).each(
+      (msg) => {
+        cy.get("nav").should("contain", msg);
+      }
+    );
+  });
+  console.log("plugin works");
+  it.skip("the sidebar directs you to the right page", () => {
+    cy.wrap(["Credentials", "Presentation", "Messages", "Connections"]).each(
+      (msg) => {
+        console.log(msg);
+        cy.get(`nav > a[href=${msg.toLowerCase()}]`).then((href) => {
+          cy.visit(href);
+          cy.url().should("include", `/${msg.toLowerCase()}`);
+        });
+      }
+    );
+  });
+  
+  console.log('plugin works')//BUG: plugin doesn't display 
+  //INFO: skipped test
+  it.skip('the sidebar directs you to the right page', () => {
+    cy.wrap(['Credentials','Presentation','Messages','Connections'])
+      .each((msg) => {
+        console.log(msg)
+        cy.get(`nav > a[href=${msg.toLowerCase()}]`)
+          .then((href) => {
+            cy.visit(href)
+            cy.url().should("include", `/${msg.toLowerCase()}`)
+           })
+      }) 
+  })
+});
