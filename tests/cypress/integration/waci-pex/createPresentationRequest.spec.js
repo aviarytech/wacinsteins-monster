bb91e415dddcd8558d0ed0ed73cd023df03f616a
Henri Vandersleyen <hvandersleyen@gmail.com>
2021-09-15 12:33:36 -0700

    started work on the first test

---
+++ tests/cypress/integration/waci-pex/createPresentationRequest.spec.js
@@ -0,0 +1,9 @@
+describe('create a presentation request', () => {
+  //assuming we are home
+  it('use the sidebar in the desktop view to visit the presentation page', () => {
+
+  })
+  it('renders all the required components for this transaction', () => {
+    console.log('test')
+  })
+})
