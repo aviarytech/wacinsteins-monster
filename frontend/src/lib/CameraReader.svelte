e4850e7949d529318fb865dd7ab594e74b40d064
Henri Vandersleyen <hvandersleyen@gmail.com>
2021-09-16 15:06:40 -0700

    binded the elements using svelte yet the js is being executed before the elements are rendered :/

--- frontend/src/lib/CameraReader.svelte
+++ frontend/src/lib/CameraReader.svelte
@@ -1,57 +1,146 @@
-<style lang="postcss">
-/*.video {
-  width: 640px;
-  height: 370px;
-  background-color: pink;
+<style>
+template {
+  font-family: "Ropa Sans", sans-serif;
+  color: #333;
+  max-width: 640px;
+  margin: 0 auto;
+  position: relative;
 }
-*/
+
+#githubLink {
+  position: absolute;
+  right: 0;
+  top: 12px;
+  color: #2d99ff;
+}
+
+h1 {
+  margin: 10px 0;
+  font-size: 40px;
+}
+
+#loadingMessage {
+  text-align: center;
+  padding: 40px;
+  background-color: #eee;
+}
+
+#canvas {
+  width: 100%;
+}
+
+#output {
+  margin-top: 20px;
+  background: #eee;
+  padding: 10px;
+  padding-bottom: 0;
+}
+
+#output div {
+  padding-bottom: 10px;
+  word-wrap: break-word;
+}
+
+#noQRFound {
+  text-align: center;
+}
 </style>
 
-<script lang="ts">
-//ECMA imports
+<script>
+//ecma scripts
 import { onMount } from "svelte";
-import swal from "sweetalert";
-//components
-import Button from "./ui/Button.svelte";
-import Image from "./table-elements/Image.svelte";
-
-let video;
-let qrScanner;
-let fileQrResult: string;
-$: fileQrResult;
-let WxH: number = 640;
-//TODO: is there a better way than using the dom?
 onMount(() => {});
 
-let clickedFlag: boolean = false;
-function startCamera() {
-  if (!true) {
-    swal({
-      title: "No camera detected",
-      text: `Please ensure a camera is available`,
-      icon: "error",
+var video = document.createElement("video");
+var canvasElement; //document.getElementById("canvas");
+var canvas = canvasElement.getContext("2d");
+var loadingMessage; //document.getElementById("loadingMessage");
+var outputContainer; // document.getElementById("output");
+var outputMessage; // document.getElementById("outputMessage");
+var outputData; // document.getElementById("outputData");
+
+// Use facingMode: environment to attemt to get the front camera on phones
+navigator.mediaDevices
+  .getUserMedia({ video: { facingMode: "environment" } })
+  .then(function (stream) {
+    video.srcObject = stream;
+    video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
+    video.play();
+    requestAnimationFrame(tick);
+  });
+
+function drawLine(begin, end, color) {
+  canvas.beginPath();
+  canvas.moveTo(begin.x, begin.y);
+  canvas.lineTo(end.x, end.y);
+  canvas.lineWidth = 4;
+  canvas.strokeStyle = color;
+  canvas.stroke();
+}
+function tick() {
+  loadingMessage.innerText = "⌛ Loading video...";
+  if (video.readyState === video.HAVE_ENOUGH_DATA) {
+    loadingMessage.hidden = true;
+    canvasElement.hidden = false;
+    outputContainer.hidden = false;
+
+    canvasElement.height = video.videoHeight;
+    canvasElement.width = video.videoWidth;
+    canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
+    var imageData = canvas.getImageData(
+      0,
+      0,
+      canvasElement.width,
+      canvasElement.height
+    );
+    var code = jsQR(imageData.data, imageData.width, imageData.height, {
+      inversionAttempts: "dontInvert",
     });
+    if (code) {
+      drawLine(
+        code.location.topLeftCorner,
+        code.location.topRightCorner,
+        "#FF3B58"
+      );
+      drawLine(
+        code.location.topRightCorner,
+        code.location.bottomRightCorner,
+        "#FF3B58"
+      );
+      drawLine(
+        code.location.bottomRightCorner,
+        code.location.bottomLeftCorner,
+        "#FF3B58"
+      );
+      drawLine(
+        code.location.bottomLeftCorner,
+        code.location.topLeftCorner,
+        "#FF3B58"
+      );
+      outputMessage.hidden = true;
+      outputData.parentElement.hidden = false;
+      outputData.innerText = code.data;
+    } else {
+      outputMessage.hidden = false;
+      outputData.parentElement.hidden = true;
+    }
   }
-  if (!clickedFlag) {
-    clickedFlag = !clickedFlag;
-  } else {
-    clickedFlag = !clickedFlag;
-  }
+  requestAnimationFrame(tick);
 }
 </script>
 
-<template
-  >{fileQrResult}
-  <Button
-    callback="{async () => startCamera()}"
-    label="start"
-    slotOverLabel="{true}">
-    <Image
-      src="./assets/svg/outlineCamera.svg"
-      alt="use Camera"
-      width="{16}"
-      height="{16}" />
-  </Button>
-  <video id="videoElem" class="video" bind:this="{video}"
-    ><track kind="captions" /></video>
+<template>
+  <div id="loadingMessage" bind:this="{loadingMessage}">
+    🎥 Unable to access video stream (please make sure you have a webcam
+    enabled)
+  </div>
+  <canvas id="canvas" bind:this="{canvasElement}" hidden></canvas>
+  <div id="output" hidden binf:this="{outputContainer}">
+    <div id="outputMessage" bind:this="{outputMessage}">
+      No QR code detected.
+    </div>
+    <div hidden>
+      <b>Data:</b> <span id="outputData" bind:this="{outputData}"></span>
+    </div>
+  </div>
 </template>
