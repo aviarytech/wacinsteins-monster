f7fb217783b85ccd266f26c286778a64c27b6644
Henri Vandersleyen <hvandersleyen@gmail.com>
2021-09-17 13:56:02 -0700

    small cleanup but the sidebar closes. One more button to tie before success.

--- frontend/src/lib/Sidebar.svelte
+++ frontend/src/lib/Sidebar.svelte
@@ -52,48 +52,15 @@ let mblSidebar = false;
   <div
     class="fixed inset-0 flex z-40 md:hidden"
     role="dialog"
-    aria-modal="true">
-    <!--
-      Off-canvas menu overlay, show/hide based on off-canvas menu state.
-
-      Entering: "transition-opacity ease-linear duration-300"
-        From: "opacity-0"
-        To: "opacity-100"
-      Leaving: "transition-opacity ease-linear duration-300"
-        From: "opacity-100"
-        To: "opacity-0"
-    -->
-    <div
-      class="fixed inset-0 bg-gray-600 bg-opacity-75"
-      aria-hidden="true"
-      class:canvas-overlay-off="{mblSidebar}"
-      class:canvas-overlay-on="{!mblSidebar}">
+    aria-modal="true"
+    class:canvas-overlay-off="{mblSidebar}"
+    class:canvas-overlay-on="{!mblSidebar}">
+    <div class="fixed inset-0 bg-gray-600 bg-opacity-75" aria-hidden="true">
     </div>
-
-    <!--
-      Off-canvas menu, show/hide based on off-canvas menu state.
-
-      Entering: "transition ease-in-out duration-300 transform"
-        From: "-translate-x-full"
-        To: "translate-x-0"
-      Leaving: "transition ease-in-out duration-300 transform"
-        From: "translate-x-0"
-        To: "-translate-x-full"
-    -->
     <div
       class="relative flex-1 flex flex-col max-w-xs w-full pt-5 pb-4 bg-gray-600"
       class:.mbl-sidebar-menu-off="{mblSidebar}"
       class:.mbl-sidebar-menu-on="{!mblSidebar}">
-      <!--
-        Close button, show/hide based on off-canvas menu state.
-
-        Entering: "ease-in-out duration-300"
-          From: "opacity-0"
-          To: "opacity-100"
-        Leaving: "ease-in-out duration-300"
-          From: "opacity-100"
-          To: "opacity-0"
-      -->
       <div class="absolute top-0 right-0 -mr-12 pt-2">
         <button
           type="button"
