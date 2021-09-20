9dd2e815bfdbc0a5a422a757f2f709968dbe6d13
Brian Richter <brianorwhatever@gmail.com>
2021-09-20 02:37:34 -0700

    new faq

--- frontend/src/routes/Demo.svelte
+++ frontend/src/routes/Demo.svelte
@@ -17,15 +17,14 @@
 }
 </style>
 
-<div class="bg-white shadow-md rounded-sm mx-auto">
-  <div class="py-4 px-8 divide-gray-200">
+<script>
+import Video from "../lib/Video.svelte";
+</script>
+
+<div class="bg-white shadow-md rounded-sm">
+  <div class="divide-gray-200">
     <div class="embed-container">
-      <iframe
-        src="https://www.youtube.com/embed/g581s942Kas?autoplay=1&loop=1&controls=0&playlist=g581s942Kas"
-        title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen></iframe>
+      <Video />
     </div>
   </div>
 </div>
