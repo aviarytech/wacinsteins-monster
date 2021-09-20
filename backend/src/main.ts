f4999da941175e2a2d90bef2ad83f969faafcef7
Brian Richter <brianorwhatever@gmail.com>
2021-09-20 11:23:02 -0700

    fixed fastify init

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,29 +4,29 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
-import fastify from 'fastify';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const server = await fastify({ ignoreTrailingSlash: true });
-
-  server.addContentTypeParser(
-    'application/didcomm-encrypted+json',
-    function (request, payload, done) {
-      var data = '';
-      payload.on('data', (chunk) => {
-        data += chunk;
-      });
-      payload.on('end', () => {
-        done(null, JSON.parse(data));
-      });
-    },
-  );
+  const adapter = new FastifyAdapter({ ignoreTrailingSlash: true });
+  adapter
+    .getInstance()
+    .addContentTypeParser(
+      'application/didcomm-encrypted+json',
+      function (request, payload, done) {
+        var data = '';
+        payload.on('data', (chunk) => {
+          data += chunk;
+        });
+        payload.on('end', () => {
+          done(null, JSON.parse(data));
+        });
+      },
+    );
 
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter(server.server),
+    adapter,
   );
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
