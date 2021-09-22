ee5bd67a458ce4ac22cd5e55c0c5789902415637
Henri Vandersleyen <hvandersleyen@gmail.com>
2021-09-22 12:53:14 -0700

    corrected route names and reintroduced hidden routes

--- frontend/src/stores/routes.ts
+++ frontend/src/stores/routes.ts
@@ -9,21 +9,21 @@ export const Routes: routeHooks[] = [
   {
     routeName: "Wallet",
     routeUrl: "credentials",
-    heroIcon: "../assets/icons/wallet.svg",
+    heroIcon: "./assets/icons/wallet.svg",
   },
   {
     routeName: "Presentations",
     routeUrl: "presentations",
-    heroIcon: "../assets/icons/inbox-full.svg",
+    heroIcon: "./assets/icons/inbox-full.svg",
   },
-  // {
-  //   routeName: "Messages",
-  //   routeUrl: "messages",
-  //   heroIcon: "../assets/icons/chat-bubble-dots.svg",
-  // },
-  // {
-  //   routeName: "Contacts", // be cool if we could use a cog
-  //   routeUrl: "contacts",
-  //   heroIcon: "../assets/icons/user-group.svg",
-  // },
+  {
+    routeName: "Messages",
+    routeUrl: "messages",
+    heroIcon: "./assets/icons/chat-bubble-dots.svg",
+  },
+  {
+    routeName: "Contacts", // be cool if we could use a cog
+    routeUrl: "contacts",
+    heroIcon: "./assets/icons/user-group.svg",
+  },
 ];
