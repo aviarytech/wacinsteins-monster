7353fb0bc14c07951218c606d0978b5776b7e377
Henri Vandersleyen <hvandersleyen@gmail.com>
2021-09-22 16:07:14 -0700

    added crypto (wasm import) to identities. Generates a xpriv key.

--- frontend/src/routes/Identities.svelte
+++ frontend/src/routes/Identities.svelte
@@ -6,12 +6,20 @@ import ComponentList from "../lib/table-elements/ComponentList.svelte";
 import Button from "../lib/ui/Button.svelte";
 import Image from "../lib/table-elements/Image.svelte";
 import Avatar from "../lib/ui/Avatar.svelte";
+import Tag from "../lib/ui/Tag.svelte";
+import QRcode from "../lib/slideOverItems/QRcode.svelte";
 //ecma imports
 import { onMount } from "svelte";
+import { random } from "jose/util/random";
+import init, {
+  ExtendedPrivateKey,
+} from "../../node_modules/bsv-wasm-web/bsv_wasm.js"; //"bsv-wasm-web";
 //stores
 import { identities } from "../stores/identities";
 import { slideOverContent } from "../stores/ui";
 import { user } from "../stores/user";
+import { qrCodeIdValue } from "../stores/presentation";
+
 //utils
 import { sha256 } from "../utils/sha256";
 //api
@@ -26,18 +34,50 @@ const openIdentity = (value: string) => {
 };
 
 onMount(async () => {
+  await init();
   let ids = [{ role: "You", id: $user.email }];
   const resp = await getServerIdentity();
   if (resp) {
-    console.log(resp.id);
+    //console.log(resp.id);
     ids = [...ids, { role: "Server", id: resp.id }];
   }
   identities.set(ids);
 });
+
+function generateXPriv(): void {
+  let small_bytes: any = random(new Uint8Array(32));
+  let xpriv_wasm = ExtendedPrivateKey.fromSeed(small_bytes);
+  qrCodeIdValue.set("hallo");
+
+  //console.log(xpriv_wasm, small_bytes);
+}
+let rightPreviewWindowDisplayed = false;
+function importXpubKeys(): void {
+  generateXPriv();
+  slideOverContent.set({
+    title: ``,
+    component: QRcode,
+    presentationSubject: [],
+  });
+  if (rightPreviewWindowDisplayed) {
+    slideOverContent.set(null);
+  }
+  rightPreviewWindowDisplayed = !rightPreviewWindowDisplayed;
+}
 </script>
 
 <!-- <pre>{JSON.stringify($identities, null, 2)}</pre> -->
 {#if $identities}
+  <Button
+    label="xPub"
+    slotOverLabel="{true}"
+    callback="{async () => importXpubKeys()}">
+    <Tag text="XPub" /><Image
+      src="./assets/icons/key.svg"
+      alt="XPub"
+      width="{16}"
+      height="{16}" />
+  </Button>
   <DataTable
     xOverflowClass="bg-red"
     headers="{['', 'Role', 'Identifier', '']}"
