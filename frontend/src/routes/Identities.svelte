643e529b6ce1268a100280cfe39bddfeb7fbd9c7
Henri Vandersleyen <hvandersleyen@gmail.com>
2021-09-23 13:20:57 -0700

    {open,close} from simple modal giving me issues. will merge main into to correct issue.

--- frontend/src/routes/Identities.svelte
+++ frontend/src/routes/Identities.svelte
@@ -8,13 +8,16 @@ import Image from "../lib/table-elements/Image.svelte";
 import Avatar from "../lib/ui/Avatar.svelte";
 import Tag from "../lib/ui/Tag.svelte";
 import QRcode from "../lib/slideOverItems/QRcode.svelte";
+import CameraReader from "../lib/CameraReader.svelte";
 //ecma imports
-import { onMount } from "svelte";
+import { onMount, getContext } from "svelte";
 import { random } from "jose/util/random";
 import init, {
   ExtendedPrivateKey,
   ExtendedPublicKey,
 } from "../../node_modules/bsv-wasm-web/bsv_wasm.js"; //"bsv-wasm-web";
+import swal from "sweetalert";
+import { debounce } from "lodash";
 //stores
 import { identities, extendedPubKeys } from "../stores/identities";
 import { slideOverContent } from "../stores/ui";
@@ -25,6 +28,7 @@ import { sha256 } from "../utils/sha256";
 //api
 import { getServerIdentity } from "../api/identities";
 
+//const { open, close } = getContext("simple-modal"); //not really an import
 const openIdentity = (value: string) => {
   slideOverContent.set({
     component: Avatar,
@@ -63,12 +67,16 @@ function generateXPriv() {
   console.log(xpriv_key, small_bytes, xpub_key.toString(), $extendedPubKeys);
   return xpub_key.toString();
 }
-function importXpubKeys(): void {
+function createXKeyPair(): void {
   slideOverContent.set({
     title: `Public Key`,
     component: QRcode,
     value: generateXPriv(),
   });
+}
+function importXKeys() {
+  console.log("here");
+  open(CameraReader);
 }
 function deleteKey(id: number) {
   //I am sure there is a more svelte way of doing this :(
@@ -123,22 +131,37 @@ function deleteKey(id: number) {
       })}" />
   </div>
   <div class="bg-white shadow-md mt-10 rounded-sm p-5">
-    <Button
-      label="New Key Pair"
-      slotOverLabel="{true}"
-      callback="{async () => importXpubKeys()}"
-      additionalClasses="mb-4">
-      <Tag text="New Key Pair" /><Image
-        src="./assets/icons/key.svg"
-        alt="Key"
-        width="{16}"
-        height="{16}" />
-    </Button>
-    <Tag
-      text="Keys"
-      fontColor="text-white"
-      bgCol="{tailwingBgColorizer('submitted')}" />
-    <span class="pl-2 flex-grow">Your Extended keys</span>
+    <div class="block">
+      <Button
+        label="New Key Pair"
+        slotOverLabel="{true}"
+        callback="{async () => createXKeyPair()}"
+        additionalClasses="mb-4">
+        <Tag text="New Key Pair" /><Image
+          src="./assets/icons/key.svg"
+          alt="Key"
+          width="{16}"
+          height="{16}" />
+      </Button>
+      <Button
+        label="Scan XQRcode"
+        slotOverLabel="{true}"
+        callback="{async () => importXKeys()}"
+        additionalClasses="mb-4">
+        <Tag text="Scan XQRcode" /><Image
+          src="./assets/icons/camera.svg"
+          alt="Key"
+          width="{16}"
+          height="{16}" />
+      </Button>
+      <div class="flex">
+        <Tag
+          text="Keys"
+          fontColor="text-white"
+          bgCol="{tailwingBgColorizer('submitted')}" />
+        <span class="pl-2 flex-grow">Your Extended keys</span>
+      </div>
+    </div>
     {#if $extendedPubKeys}
       <DataTable
         xOverflowClass="bg-red"
