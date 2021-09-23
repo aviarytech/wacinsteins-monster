cc20275eaaa2df0c61a3ffc2e5c3c201fdd6d224
Henri Vandersleyen <hvandersleyen@gmail.com>
2021-09-23 12:29:31 -0700

    Created the framework to generate a pub/priv key pair and delete it. All keys can be represented by a qrCode.

--- frontend/src/routes/Identities.svelte
+++ frontend/src/routes/Identities.svelte
@@ -16,13 +16,11 @@ import init, {
   ExtendedPublicKey,
 } from "../../node_modules/bsv-wasm-web/bsv_wasm.js"; //"bsv-wasm-web";
 //stores
-import { identities } from "../stores/identities";
+import { identities, extendedPubKeys } from "../stores/identities";
 import { slideOverContent } from "../stores/ui";
 import { user } from "../stores/user";
 //utils
 import { tailwingBgColorizer } from "../utils/tailwind";
-
-//utils
 import { sha256 } from "../utils/sha256";
 //api
 import { getServerIdentity } from "../api/identities";
@@ -46,35 +44,41 @@ onMount(async () => {
   identities.set(ids);
 });
 
+//XPub/Priv keys
 function generateXPriv() {
+  //rng
   let small_bytes: any = random(new Uint8Array(32));
   let xpriv_key = ExtendedPrivateKey.fromSeed(small_bytes);
   let xpub_key = ExtendedPublicKey.fromXPriv(xpriv_key);
-  console.log(xpriv_key, small_bytes, xpub_key.toString());
+  let lenKeyChain: number = $extendedPubKeys ? $extendedPubKeys.length : 0;
+  let keyChain = [
+    ...$extendedPubKeys,
+    {
+      id: lenKeyChain,
+      privKey: xpriv_key.toString(),
+      pubKey: xpub_key.toString(),
+    },
+  ];
+  extendedPubKeys.set(keyChain);
+  console.log(xpriv_key, small_bytes, xpub_key.toString(), $extendedPubKeys);
   return xpub_key.toString();
 }
 function importXpubKeys(): void {
   slideOverContent.set({
-    title: ``,
+    title: `Public Key`,
     component: QRcode,
     value: generateXPriv(),
   });
+}
+function deleteKey(id: number) {
+  //I am sure there is a more svelte way of doing this :(
+  let res = $extendedPubKeys.filter((item) => item.id !== id);
+  extendedPubKeys.set(res);
 }
 </script>
 
 <!-- <pre>{JSON.stringify($identities, null, 2)}</pre> -->
 {#if $identities}
-  <Button
-    label="xPub"
-    slotOverLabel="{true}"
-    callback="{async () => importXpubKeys()}"
-    additionalClasses="mb-4">
-    <Tag text="XPub" /><Image
-      src="./assets/icons/key.svg"
-      alt="XPub"
-      width="{16}"
-      height="{16}" />
-  </Button>
   <div class="bg-white shadow-md rounded-sm p-5">
     <Tag
       text="You"
@@ -119,10 +123,65 @@ function importXpubKeys(): void {
       })}" />
   </div>
   <div class="bg-white shadow-md mt-10 rounded-sm p-5">
+    <Button
+      label="New Key Pair"
+      slotOverLabel="{true}"
+      callback="{async () => importXpubKeys()}"
+      additionalClasses="mb-4">
+      <Tag text="New Key Pair" /><Image
+        src="./assets/icons/key.svg"
+        alt="Key"
+        width="{16}"
+        height="{16}" />
+    </Button>
     <Tag
       text="Keys"
       fontColor="text-white"
       bgCol="{tailwingBgColorizer('submitted')}" />
     <span class="pl-2 flex-grow">Your Extended keys</span>
+    {#if $extendedPubKeys}
+      <DataTable
+        xOverflowClass="bg-red"
+        headers="{['ID', 'XPriv', 'XPub', 'actions']}"
+        data="{$extendedPubKeys.map((i) => {
+          return [
+            { component: Text, text: i.id },
+            { component: Text, text: i.privKey },
+            { component: Text, text: i.pubKey },
+            {
+              component: ComponentList,
+              items: [
+                {
+                  component: Button,
+                  label: 'XPriv',
+                  callback: () => {
+                    slideOverContent.set({
+                      title: `Extended Private Key`,
+                      component: QRcode,
+                      value: i.privKey,
+                    });
+                  },
+                },
+                {
+                  component: Button,
+                  label: 'XPub',
+                  callback: () => {
+                    slideOverContent.set({
+                      title: `Extended Public Key`,
+                      component: QRcode,
+                      value: i.pubKey,
+                    });
+                  },
+                },
+                {
+                  component: Button,
+                  callback: () => deleteKey(i['id']),
+                  label: 'Delete',
+                },
+              ],
+            },
+          ];
+        })}" />
+    {/if}
   </div>
 {/if}
